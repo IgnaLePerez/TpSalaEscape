8b920378c2ca90b1e1db152d5e09131eac2b8cc8
IGNACIO LEÓN PEREZ <49961152@ort>
2026-09-08 13:07:41 -0300

    8/9

--- obj/Debug/net9.0/tpSalaDeEscape.AssemblyInfo.cs
+++ obj/Debug/net9.0/tpSalaDeEscape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpSalaDeEscape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2551aee1bb2ae032933392abd3fffb5138dce5a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa3804d10f922e77a15f978f2578d08870eaff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpSalaDeEscape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpSalaDeEscape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
