8f521f31687f67f9a7df414da90a0f5e3fcc865f
CAMILA BELEN STANCANELLI <49926825@ort>
2026-09-14 14:29:59 -0300

    bntSaltear

--- wwwroot/js/dialogo4.js
+++ wwwroot/js/dialogo4.js
@@ -1,3 +1,5 @@
+btnSaltear = document.getElementById("btnSaltear");
+
 const dialogos = [
     "Pero todavía no terminamos",
     "Cof, cof. Me muero de sed",
@@ -13,5 +15,11 @@ function AvanzarDialogo() {
     if (indiceDialogo < dialogos.length) {
         txtDialogo.innerHTML = dialogos[indiceDialogo];
         indiceDialogo++;
+        if (indiceDialogo === dialogos.length) {
+          btnSaltear.style.display = "none";
+        }
+    }
+    else{
+      window.location.href = '/home/IrASala';
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,6 +3,7 @@ btnOpcion1 = document.getElementById("btnOpcion1");
 btnOpcion2 = document.getElementById("btnOpcion2");
 btnOpcion3 = document.getElementById("btnOpcion3");
 btnOpcion4 = document.getElementById("btnOpcion4");
+btnSaltear = document.getElementById("btnSaltear");
 mensajeError = document.getElementById("mensajeError");
 const contenedorError = document.querySelector(".error-container");
 
@@ -65,6 +66,12 @@ function AvanzarDialogo() {
     if (indiceDialogo < dialogos.length) {
         txtDialogo.innerHTML = dialogos[indiceDialogo];
         indiceDialogo++;
+        if (indiceDialogo === dialogos.length) {
+          btnSaltear.style.display = "none";
+        }
+    }
+    else{
+      window.location.href = '/home/IrASala';
     }
 }   
 
